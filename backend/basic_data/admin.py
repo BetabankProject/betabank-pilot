e1589eeb517245c7d2f1f0cb5d79d7aa7d884a48
Aris Papadopoulos <Aristides.PAPADOPOULOS@sword-group.com>
2015-12-06 15:19:32 +0100

    Fixed the enums, and other small fixes

--- backend/basic_data/admin.py
+++ backend/basic_data/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
 from .models import Profile
 from .models import SkillFamily
 from .models import Skill
 from .models import InitialCapital
 from .models import Request
+from .models import Offer
 
 
 admin.site.register(Profile)
@@ -13,3 +17,22 @@ admin.site.register(SkillFamily)
 admin.site.register(Skill)
 admin.site.register(InitialCapital)
 admin.site.register(Request)
+admin.site.register(Offer)
+
+
+# Define an inline admin descriptor for Profile model
+# which acts a bit like a singleton
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    # verbose_name_plural = 'employee'
+
+
+# Define a new User admin
+class CustomUserAdmin(UserAdmin):
+    inlines = (ProfileInline, )
+
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
--- backend/basic_data/models.py
+++ backend/basic_data/models.py
@@ -8,16 +8,25 @@ class SkillFamily(models.Model):
     A broad family of skills eg Arts&Crafts, Business etc
     """
     title = models.CharField(max_length=100)
-    description = models.CharField(max_length=200)
+    description = models.TextField(max_length=500)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = "Skill Families"
 
 
 class Skill(models.Model):
     """
     A detailed skill, that belongs to a :model:`models.SkillFamily`
     """
-    family = models.ForeignKey(SkillFamily)
+    family = models.ForeignKey(SkillFamily, verbose_name='Family')
     title = models.CharField(max_length=100)
-    description = models.CharField(max_length=200)
+    description = models.TextField(max_length=500)
+
+    def __str__(self):
+        return "{} - {}".format(self.family.title, self.title)
 
 
 class Profile(models.Model):
@@ -42,7 +51,7 @@ class InitialCapital(models.Model):
     (by checking, the sum of all values of this table to the
     sum of the current balance of all users.
     """
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(User, verbose_name='User')
     value = models.PositiveSmallIntegerField()
 
 
@@ -51,10 +60,15 @@ class RequestStatus(enum.Enum):
     """
     Enum that defines the statuses in which a request can be.
     """
-    OPEN = 'O'
-    CANCELLED = 'C'
-    MATCHED = 'M'
-    FINISHED = 'F'
+    OPEN = 0
+    CANCELLED = 1
+    MATCHED = 2
+    FINISHED = 3
+
+    # TODO add labels in a i18n way
+    labels = {
+        OPEN: 'OPEN',
+    }
 
     _transitions = {
         OPEN: (CANCELLED, MATCHED,),
@@ -66,10 +80,55 @@ class RequestStatus(enum.Enum):
 
 
 class Request(models.Model):
-    user = models.ForeignKey(User)
-    date = models.DateTimeField("Date posted")
+    """
+    Represents a request of a service, placed by a user (an ad).
+    Users that are interested in providing the requested service, can respond by placing an :model:`models.Offer`.
+    The request can be for a fixed number of hours, or it can be an "open request", in which case the users
+    can include the suggested amount of hours in their :model:`models.Offer`.
+    """
+    user = models.ForeignKey(User, verbose_name='Placed by')
+    date = models.DateTimeField('Date posted')
+    title = models.CharField(max_length=200)
     description = models.TextField(max_length=2000)
+    category = models.ForeignKey(SkillFamily)
+    hours = models.PositiveSmallIntegerField('Number of hours required, 0 if open request')
     status = enum.EnumField(RequestStatus)
 
 
-# TODO add transaction
+class OfferStatus(enum.Enum):
+    """
+    Enum that defines the statuses in which a request can be
+    """
+    PLACED = 0
+    CANCELLED = 1
+    ACCEPTED = 2
+    REJECTED = 3
+    FINISHED = 4
+
+    # TODO add labels in a i18n way
+    labels = {
+        PLACED: 'PLACED',
+    }
+
+    _transitions = {
+        PLACED: (CANCELLED, ACCEPTED, REJECTED),
+        ACCEPTED: (FINISHED,),
+        CANCELLED: (),
+        REJECTED: (),
+        FINISHED: (),
+    }
+
+
+class Offer(models.Model):
+    """
+    An offer is a response to a :model:`models.Request`, placed by a user who is willing to offer the
+    service that is requested.
+    If the request was an open one, the user must provide the number of hours.
+    """
+    placedBy = models.ForeignKey(User, verbose_name='Placed by')
+    date = models.DateTimeField('Date placed')
+    request = models.ForeignKey(Request)
+    description = models.TextField(max_length=2000)
+    hours = models.PositiveSmallIntegerField('Number of hours (only to be filled if the offer is for an open request)')
+    status = enum.EnumField(OfferStatus)
+
